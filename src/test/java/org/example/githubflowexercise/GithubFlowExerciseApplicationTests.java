c8704025406c541456ec17e0094e254d7e119be5
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-02-17 13:10:57 +0100

    initial commit

---
+++ src/test/java/org/example/githubflowexercise/GithubFlowExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubflowexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubFlowExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
